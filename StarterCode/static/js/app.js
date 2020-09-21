6dadcc32af63fece7ad9b661874cda64772bec95
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-09-21 14:41:31 -0500

    adding plotly bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,14 +2,17 @@
 d3.json("samples.json").then(function(data) {
   let sel_id = "selDataset";
   let metaData = data['metadata'];
-  //console.log(metaData, samplesData);
+  let samplesData = data['samples'];
+  console.log(samplesData);
   initDropdownList(sel_id, metaData);
+  buildTable(metaData[0]);
+  buildPlot(samplesData[0]['otu_ids'], samplesData[0]['sample_values'],
+            samplesData[0]['otu_labels']);
 });
 
 function optionChanged (select_value) {
     alert ("The selected option is " + select_value);
     d3.json("samples.json").then(function(data) {
-      let sel_id = "selDataset";
       let info_id = "sample-metadata";
       let metaData = data['metadata'];
       let samplesData = data['samples'];
@@ -22,6 +25,34 @@ function optionChanged (select_value) {
               }
           }
       });
+};
+
+function buildPlot(data_labels, data_values, text_values) {
+  var top_values, top_labels, top_text_values;
+  var myText = 'My text';
+  for(var i = 0; i < data_labels.length; i += 1){
+    data_labels[i] = 'OTU ' + data_labels[i];
+  }
+  console.log(data_values, data_labels);
+  top_values = data_values.sort((a, b) => b - a).slice(0, 10);
+  top_labels = data_labels.slice(0, 10);
+  top_text_values = text_values.slice(0, 10);
+  console.log(top_values, data_values.slice(0, 10));
+  var data = [{
+    x: top_values,
+    y: top_labels,
+    text: top_text_values,
+    hovertemplate: ' x: %{x}<br> y: %{y}<br> Placement:%{text}<br> myText: ' + myText,
+    type: "bar",
+    orientation: 'h'
+  }];
+
+  var layout = {
+    height: 500,
+    width: 400
+  };
+
+  Plotly.newPlot("bar", data, layout);
 };
 
 function buildTable(dates) {
